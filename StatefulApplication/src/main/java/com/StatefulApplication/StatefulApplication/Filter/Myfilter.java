1a7c8e9dd8a02cbcd585cc29992ceae5d4a879ab
mahendraptdr <122710014+mahendraptdr@users.noreply.github.com>
2024-08-14 17:28:41 +0530

    Update Myfilter.java

--- StatefulApplication/src/main/java/com/StatefulApplication/StatefulApplication/Filter/Myfilter.java
+++ StatefulApplication/src/main/java/com/StatefulApplication/StatefulApplication/Filter/Myfilter.java
@@ -46,7 +46,7 @@ public class Myfilter implements Filter {
                 }
                 }
             }
-            if (sessionid == true && request.getRequestURI().equals("/login") || request.getrequestURI().equals("/")||request.getRequestURI().equals("/veryfication"))
+            if (sessionid == true && request.getRequestURI().equals("/login") || request.getRequestURI().equals("/")||request.getRequestURI().equals("/veryfication"))
             {
                 response.sendRedirect(request.getContextPath()+"/home");
                 return;
